7cd20c4cc44717dc5e563bec8498c1ace03a82f9
Jesse Young <young295@purdue.edu>
2019-07-01 22:02:10 -0400

    ad quantize logic

--- src/quantize.mjs
+++ src/quantize.mjs
@@ -57,7 +57,6 @@ const getImage = async () => {
 // pull the image data array from the photo
 // convert that array into array of pixel objects
 const getPixelArray = async rgbArray => {
-  console.log(rgbArray[0])
   let pixelArray = []
   let pixelObj = {}
   for (const i in rgbArray) {
@@ -79,5 +78,78 @@ const getPixelArray = async rgbArray => {
 export const quantize = async () => {
   const rgbArray = await getImage()
   const pixelArray = await getPixelArray(rgbArray)
-  console.log(pixelArray)
+
+  const swatch = buildSwatchRecursive(pixelArray, 0, 3)
+  return swatch
+}
+
+const buildSwatchRecursive = (pixelArray, currDepth, maxDepth) => {
+  if (currDepth === maxDepth) {
+    // set up swatch
+    const swatchColor = pixelArray.reduce(
+      (prev, curr) => {
+        prev.r += curr.r
+        prev.g += curr.g
+        prev.b += curr.b
+        return prev
+      },
+      {
+        r: 0,
+        g: 0,
+        b: 0
+      }
+    )
+
+    swatchColor.r = Math.round(swatchColor.r / pixelArray.length)
+    swatchColor.g = Math.round(swatchColor.g / pixelArray.length)
+    swatchColor.b = Math.round(swatchColor.b / pixelArray.length)
+
+    return [swatchColor]
+  } else {
+    // first find which color to sort by
+    const sortColor = findLargeRange(pixelArray)
+
+    pixelArray.sort((pixel1, pixel2) => {
+      return pixel1[sortColor] - pixel2[sortColor]
+    })
+
+    const split = pixelArray.length / 2
+    const swatch = [
+      ...buildSwatchRecursive(pixelArray.slice(0, split), currDepth + 1, maxDepth),
+      ...buildSwatchRecursive(pixelArray.slice(split + 1), currDepth + 1, maxDepth)
+    ]
+    return swatch
+  }
+}
+
+const findLargeRange = pixelBucket => {
+  let rMin = 255
+  let rMax = 0
+
+  let gMin = 255
+  let gMax = 0
+
+  let bMin = 255
+  let bMax = 0
+
+  for (const pixel of pixelBucket) {
+    rMin = rMin < pixel.r ? rMin : pixel.r
+    rMax = rMax > pixel.r ? rMax : pixel.r
+    gMin = gMin < pixel.g ? gMin : pixel.g
+    gMax = gMax > pixel.g ? gMax : pixel.g
+    bMin = bMin < pixel.b ? bMin : pixel.b
+    bMax = bMax > pixel.b ? bMax : pixel.b
+  }
+
+  const rRange = rMax - rMin
+  const gRange = gMax - gMin
+  const bRange = bMax - bMin
+
+  if (rRange > gRange && rRange > bRange) {
+    return 'r'
+  } else if (gRange > rRange && gRange > bRange) {
+    return 'g'
+  } else {
+    return 'b'
+  }
 }
